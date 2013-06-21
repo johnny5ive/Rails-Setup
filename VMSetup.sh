f104dce8f02ebe69f938bccac3a708acd75ed422
Morgan Hein <code@morganhein.com>
2013-06-21 12:21:52 -0700

    Added colors to VM, updated guest-additions package

--- VMSetup.sh
+++ VMSetup.sh
@@ -1,12 +1,37 @@
-aptitude install -y aptitude
-aptitude update && aptitude safe-upgrade
+# Text color variables
+und=$(tput sgr 0 1)			# Underline
+bold=$(tput bold)           # Bold
+red=$(tput setaf 1) 		# Red
+blue=$(tput setaf 4) 		# Blue
+wht=$(tput setaf 7) 		# White
+reset=$(tput sgr0)          # Reset
+redbold=${red}${bold}
+
+echo -e $redbold"Updating System\n"$reset
+aptitude update && aptitude safe-upgrade > /dev/null
+
+echo -e $blue"Installing python-software-properties"$reset
 aptitude install -y python-software-properties
+echo -e $blue"Done.\n"$reset
+
 add-apt-repository ppa:gnome3-team/gnome3
 aptitude update
-aptitude install -y gnome-shell
-aptitude install -y lightdm
+
+echo -e $blue"Installing lightdm, gnome-shell"$reset
+aptitude install -y lightdm gnome-shell > /dev/null
+echo -e $blue"Done.\n"$reset
+
+echo -e $blue"Configuring lightdm"$reset
 dpkg-reconfigure lightdm
-aptitude install -y gnome-terminal gnome-tweak-tool gnome-themes-ubuntu gedit evince file-roller gnome-utils gdebi update-manager gnome-themes gnome-themes-extras ubuntu-artwork
-aptitude -y install virtualbox-ose-guest-utils
-aptitude -y -f install
-aptitude clean
+echo -e $blue"Done.\n"$reset
+
+echo -e $blue"Installing extra software"$reset
+aptitude install -y gnome-terminal gnome-tweak-tool gnome-themes-ubuntu gedit evince file-roller gnome-utils gdebi update-manager gnome-themes gnome-themes-extras ubuntu-artwork > /dev/null
+aptitude -y install virtualbox-guest-utils > /dev/null
+echo -e $blue"Done.\n"$reset
+
+echo -e $blue"Cleaning up"$reset
+aptitude -y -f install > /dev/null
+aptitude clean > /dev/null
+echo -e $blue"Done.\n"$reset
+reboot
