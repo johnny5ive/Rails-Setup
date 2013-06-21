af42bf9f17202a840fe8300fd71e2f2242fb4195
Morgan Hein <code@morganhein.com>
2013-06-21 11:59:17 -0700

    Renamed MinimalSetup to VMSetup

--- VMSetup.sh
+++ VMSetup.sh
@@ -7,5 +7,6 @@ aptitude install -y gnome-shell
 aptitude install -y lightdm
 dpkg-reconfigure lightdm
 aptitude install -y gnome-terminal gnome-tweak-tool gnome-themes-ubuntu gedit evince file-roller gnome-utils gdebi update-manager gnome-themes gnome-themes-extras ubuntu-artwork
+aptitude -y install virtualbox-ose-guest-utils
 aptitude -y -f install
 aptitude clean
