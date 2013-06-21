a0788b9538c2ea2f00c3a70460b390e5eac89306
Morgan Hein <code@morganhein.com>
2013-06-20 17:17:06 -0700

    Updated to dev setup, included Node, NPM, Yeoman.

--- DevSetup.sh
+++ DevSetup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #
-# Johnny5ive's Ruby on Rails auto-config script.
-#
+# Created by Morgan Hein
+# A Ruby, RoR, Node/NPM install and config script.
+
+
+#Ruby version to in install and use as default
 #Set this to whatever version you want (NO SPACES!)
-RUBYVERSION="1.9.3-p194"
+RUBYVERSION="2.0.0-p195"
 
 # Text color variables
 und=$(tput sgr 0 1)             # Underline
@@ -14,7 +17,7 @@ reset=$(tput sgr0)              # Reset
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-echo -e $red'This program will install all necessary dependencies for Ruby, RubyGems, and Rails.\nIt also installs several gems.'$reset
+echo -e $red'This program will install all necessary dependencies for Ruby, RubyGems, Rails, Node.js, and NPM.\nIt also installs several gems and bundles.'$reset
 echo -e $red$bold'You will be prompted for your sudo password.'$reset
 
 # clear any previous sudo permission
@@ -23,13 +26,13 @@ sudo -k
 # run inside sudo
 sudo sh <<SCRIPT
 sudo apt-get update
-sudo apt-get install -y build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
+sudo apt-get install -y build-essential git openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config
 SCRIPT
 
-# clear sudo again to install Ruby
+# clear sudo to continue
 sudo -k
 
-echo -e $red'Installing dependencies completed. Dropping sudo to install Ruby.\n'$reset
+echo -e $red'Installing dependencies completed. Dropping sudo to continue.\n'$reset
 
 echo -e $red'Making Directories'$reset
 if [ ! -d "$HOME/.rbenv" ]; then
@@ -39,6 +42,13 @@ fi
 	
 echo 'Creating .temp'
 mkdir $DIR/.temp
+
+echo -e $red'\nSetting up Git'$reset
+git config --global user.email "code@morganhein.com"
+git config --global user.name "Morgan Hein"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+git config --global push.default simple
 
 echo -e $red'\nInstalling RBEnv'$reset
 git clone git://github.com/sstephenson/rbenv.git $HOME/.rbenv
@@ -55,8 +65,9 @@ mkdir -p ~/.rbenv/plugins
 git clone git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
 echo -e $red'Done\n'$reset
 
-echo -e $red'Installing Ruby'$reset
-$HOME/.rbenv/bin/rbenv install $RUBYVERSION
+echo -e $red'Installing Ruby\n'$reset
+$HOME/.rbenv/bin/rbenv install --verbose $RUBYVERSION
+echo -e $red'Rehashing rbenv'$reset
 $HOME/.rbenv/bin/rbenv rehash
 echo -e $red'Done\n'$reset
 
@@ -67,20 +78,39 @@ ruby -v
 cd $DIR/.temp
 
 echo -e $red'\nInstalling RubyGems'$reset
-wget -c http://production.cf.rubygems.org/rubygems/rubygems-1.8.24.tgz
-tar -xzf rubygems-1.8.24.tgz
-cd rubygems-1.8.24
+git clone https://github.com/rubygems/rubygems.git
+cd rubygems
 ruby setup.rb
 echo -e $red'Done'$reset
 
 echo -e $red'\nSetting up .gemrc file.\n'$bold'By default this .gemrc does not download docs for gems.'$reset
-cd ../
 mv $DIR/.gemrc $HOME/.gemrc
 echo -e $red'Done'$reset
 
 echo -e $red'\nInstalling Rails & other gems'$reset
 gem install rails heroku foreman spork guard-spork guard bundle
 echo -e $red'Done\n\n'$reset
+
+echo -e $red'\nInstalling Node.js'$reset
+cd $DIR/.temp
+git clone https://github.com/joyent/node.git
+cd node
+
+./configure
+make
+sudo make install
+
+node -v
+echo -e $red'Done\n\n'$reset
+
+echo -e $red'\nInstalling NPM'$reset
+cd $DIR/.temp
+curl http://npmjs.org/install.sh | sudo sh
+echo -e $red'Done\n\n'$reset
+
+echo -e $red'\nInstalling Yeoman and gems'$reset
+gem update --system
+npm install -g yo compass grunt-cli bower generator-angular
 
 echo -e $red$bold'Finished. Files still exist in '$DIR'/.temp. Remove them if you wish!'$reset
 echo -e $red$bold'A restart may be required for all paths to be found, or you can type: '%reset'source ~/.bash_profile'$red$bold' to begin working now.'$reset
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1> Ruby, RubyGems, and Ruby on Rails installer</h1>
+<h1> Ruby, RubyGems, RoR, Node.js, NPM, and Yeoman installer</h1>
 
 Intended for Debian based systems only (Ubuntu, Mint, etc)
 
@@ -7,12 +7,15 @@ This script installs Ruby, Rails, and Rubygems using the following mechanisms:
 1. Downloads all the dependencies using apt-get. 
 2. Downloads and installs rbenv and the ruby-build plugin to $HOME/.rbenv.
 3. Updates $PATH to include .rbenv/bin, .gems/bin, .rbenv/shims, and . (current directory)
-4. Installs Ruby "1.9.3-p194" using the ruby-build to $HOME/.rbenv/versions
-5. Installs RubyGems 1.8.24
+4. Installs Ruby "2.0.0-p194" using the ruby-build to $HOME/.rbenv/versions
+5. Installs latest RubyGems
 6. Configures a .gemrc that disables all documentation.
-7. Installs the gems: rails heroku foreman spork guard-spork guard bundle.
+7. Installs Ruby gems: rails heroku foreman spork guard-spork guard bundle.
+8. Installs latest Node.js
+9. Installs latest NPM
+10. Installs Node gems: yo, compass, grunt-cli, bower, angular.
 
 
 To install this, copy/paste into a terminal:
 
-git clone https://github.com/johnny5ive/Rails-Setup.git && chmod +x Rails-Setup/RailsSetup.sh && ./Rails-Setup/RailsSetup.sh
+git clone https://github.com/morganhein/Dev-Setup.git && chmod +x Dev-Setup/DevSetup.sh && ./Dev-Setup/DevSetup.sh
