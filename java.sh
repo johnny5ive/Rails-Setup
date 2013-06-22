6681a14b8dabf9cb33d6f794a64e33490ea89b80
Morgan Hein <code@morganhein.com>
2013-06-21 18:49:06 -0700

    Trying to get java updater to work.

--- java.sh
+++ java.sh
@@ -4,16 +4,17 @@
 #	
 echo "* Installing Sun Java. Press Enter to Continue..."
 read dummy
+echo "Continuing..."
 
-cd ~/
 wget https://github.com/flexiondotorg/oab-java6/raw/0.2.8/oab-java.sh -O oab-java.sh > /dev/null
 chmod +x oab-java.sh > /dev/null
 sudo ./oab-java.sh
 
 # aptitude -y install python-software-properties > /dev/null
 # add-apt-repository ppa:sun-java-community-team/sun-java6 > /dev/null
-# aptitude -y install sun-java6-jdk sun-java5-jdk
-update-java-alternatives -s java-1.5.0-sun > /dev/null
+
+aptitude -y install sun-java6-jdk sun-java5-jdk
+update-java-alternatives -s java-1.6.0-sun
 echo "* Install completed!"
 sudo -k
 exit 0
