1aaeaff214c9418d2776575e54477c748fd8bba2
Morgan Hein <code@morganhein.com>
2013-06-21 20:44:49 -0700

    Added -e to echo, exits cleanly.

--- extras.sh
+++ extras.sh
@@ -11,19 +11,21 @@ else
 	exit 0
 fi
 
-add-apt-repository "deb http://linux.dropbox.com/ubuntu maverick main" > /dev/null				
+add-apt-repository "deb http://linux.dropbox.com/ubuntu maverick main" > /dev/null	
+aptitude install gdebi -y > /dev/null			
 
-echo "* Installing Extras. Press Enter to Continue...\n"
+echo -e '* Installing Extras. Press Enter to Continue...\n'
 read dummy
-echo "* Installing Dropbox\n"
+echo -e '* Installing Dropbox\n'
 aptitude -y install nautilus-dropbox > /dev/null
 
-echo "* Installing Google Chrome\n"
+echo -e '* Installing Google Chrome\n'
 aptitude -y install libcurl3 xdg-utils > /dev/null
 if [ "$platform" == "i686" ]; then
 	wget -q https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_i386.deb
-	dpkg -i google-chrome-stable_current_i386.deb
+	gdebi -n google-chrome-stable_current_i386.deb
 elif [ "$platform" == "x86_64" ]; then
 	wget -q https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	dpkg -i google-chrome-stable_current_amd64.deb > /dev/null
+	gdebi -n google-chrome-stable_current_amd64.deb > /dev/null
 fi
+exit 0
