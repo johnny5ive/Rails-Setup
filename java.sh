2cb7888a094d8ce47ca7ba1e092fa20a41fd371d
Morgan Hein <code@morganhein.com>
2013-06-22 11:29:11 -0700

    Changed pulling Java script to personal repository.

--- java.sh
+++ java.sh
@@ -6,13 +6,15 @@ echo "* Installing Sun Java. Press Enter to Continue..."
 read dummy
 echo "Continuing..."
 
-wget https://github.com/flexiondotorg/oab-java6/raw/0.2.8/oab-java.sh -O oab-java.sh > /dev/null
+# aptitude install -y libdistro-info-perl > /dev/null
+
+wget https://github.com/morganhein/oab-java6/raw/0.2.8/oab-java.sh -O oab-java.sh > /dev/null
 chmod +x oab-java.sh > /dev/null
 sudo ./oab-java.sh
 
 # aptitude -y install python-software-properties > /dev/null
 # add-apt-repository ppa:sun-java-community-team/sun-java6 > /dev/null
-
+aptitude update
 aptitude -y install sun-java6-jdk
 update-java-alternatives -s java-6-sun
 echo "* Install completed!"
