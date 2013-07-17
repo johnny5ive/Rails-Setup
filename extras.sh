0b3b8c6aaea8205de7bffc275b7ad6e12be5f6b5
morganhein <github@morganhein.com>
2013-07-17 16:56:52 -0700

    Piped output to /dev/null

--- extras.sh
+++ extras.sh
@@ -23,7 +23,7 @@ echo -e '* Installing Google Chrome\n'
 aptitude -y install libcurl3 xdg-utils > /dev/null
 if [ "$platform" == "i686" ]; then
 	wget -q https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_i386.deb
-	gdebi -n google-chrome-stable_current_i386.deb
+	gdebi -n google-chrome-stable_current_i386.deb > /dev/null
 elif [ "$platform" == "x86_64" ]; then
 	wget -q https://dl-ssl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 	gdebi -n google-chrome-stable_current_amd64.deb > /dev/null
